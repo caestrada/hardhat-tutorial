410748342c9615851a5990c81384eb109b93d9f9
Carlos Estrada <carlos.estrada@coretechnologies.io>
2022-06-28 11:24:31 -0400

    Add console.logs

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,6 +4,8 @@
 // It will be used by the Solidity compiler to validate its version.
 pragma solidity 0.8.4;
 
+import "hardhat/console.sol";
+
 // This is the main building block for smart contracts.
 contract Token {
     // Some string type variables to identify the token.
@@ -39,6 +41,8 @@ contract Token {
      * the contract.
      */
     function transfer(address to, uint256 amount) external {
+        console.log("Sender balance is %s tokens", balances[msg.sender]);
+        console.log("Trying to send %s tokens to %s", amount, to);
         // Check if the transaction sender has enough tokens.
         // If `require`'s first argument evaluates to `false` then the
         // transaction will revert.
